f98abe1614f10c7f2de243eebcea27808d46d58b
Daniel Yount <TheDanYount@gmail.com>
2024-07-11 21:06:47 +0000

    Conpleted the exercise

---
+++ array-manipulation-1/src/reverse.ts
@@ -0,0 +1,15 @@
+/* exported reverse */
+/* Pseudo code practice:
+Define a function, reverse, with 1 param: an unknown array, that returns an unknown array
+Create new array of type unknown
+Create for loop that iterates over param array
+Within each loop unshift items from the param array to the new array
+After the loop return the new array */
+
+function reverse(array: unknown[]): unknown[] {
+  const newArr: unknown[] = [];
+  for (let i = 0; i < array.length; i++) {
+    newArr.unshift(array[i]);
+  }
+  return newArr;
+}
