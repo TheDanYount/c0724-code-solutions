357222322eb8ee31f79a17d4fe180b2c7050a834
Daniel Yount <TheDanYount@gmail.com>
2024-07-11 21:53:55 +0000

    Completed the exercise

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,6 +1,6 @@
 'use strict';
 /* exported getWords */
-/*Pseudo-code practice:
+/* Pseudo-code practice:
 Define a function, getWords, with one parameter, string of type string, and returns an array of strings
 Check if the parameter is not empty. If so:
 Assign the split method of the parameter, with the argument " ", to an array of strings
