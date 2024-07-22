39bfa29c4b2e8f38c183a1a1b6e098946dd5da49
Daniel Yount <TheDanYount@gmail.com>
2024-07-22 21:18:08 +0000

    Completed the exercise

---
+++ dom-manipulation/main.ts
@@ -0,0 +1,27 @@
+const $hotButton = document.querySelector('.hot-button');
+const $clickCount = document.querySelector('.click-count');
+let clickCount = 0;
+if (!$hotButton) {
+  throw new Error('Hot button not found!');
+}
+$hotButton.addEventListener('click', clickHandler);
+function clickHandler(): void {
+  if (!$clickCount || !$hotButton) {
+    throw new Error('Hot button or click count not found!');
+  }
+  clickCount++;
+  $clickCount.textContent = `Clicks: ${clickCount}`;
+  if (clickCount < 4) {
+    $hotButton.className = 'hot-button cold';
+  } else if (clickCount < 7) {
+    $hotButton.className = 'hot-button cool';
+  } else if (clickCount < 10) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (clickCount < 13) {
+    $hotButton.className = 'hot-button warm';
+  } else if (clickCount < 16) {
+    $hotButton.className = 'hot-button hot';
+  } else {
+    $hotButton.className = 'hot-button nuclear';
+  }
+}
