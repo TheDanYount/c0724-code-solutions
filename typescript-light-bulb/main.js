34d1ae47bd613a1f1fee0cfb5f16982d3aaec5d8
Daniel Yount <TheDanYount@gmail.com>
2024-07-23 21:05:59 +0000

    Completed the exercise

---
+++ typescript-light-bulb/main.js
@@ -0,0 +1,19 @@
+'use strict';
+const $bulb = document.querySelector('.bulb');
+const $background = document.querySelector('.background');
+let lightStatus = 'off';
+if (!$bulb) throw new Error('Someone had better hurry over to Home Depot!');
+$bulb.addEventListener('click', clickHandler);
+function clickHandler() {
+  if (!$bulb) throw new Error('Someone had better hurry over to Home Depot!');
+  if (!$background) throw new Error("You're off the map!");
+  if (lightStatus === 'off') {
+    lightStatus = 'on';
+    $bulb.className = 'bulb bulb-on';
+    $background.className = 'background background-on';
+  } else {
+    lightStatus = 'off';
+    $bulb.className = 'bulb bulb-off';
+    $background.className = 'background background-off';
+  }
+}
