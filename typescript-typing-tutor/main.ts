a629f96d18cf5d042293215ef88868e0b2f0c72c
Daniel Yount <TheDanYount@gmail.com>
2024-07-24 19:38:04 +0000

    Create function to earn and show award

--- typescript-typing-tutor/main.ts
+++ typescript-typing-tutor/main.ts
@@ -2,15 +2,16 @@ const sentences = [
   `Hold your ground! Hold your ground! Sons of Gonder, of Rohan, my brothers, I see in your eyes the same fear that would take the heart of me. A day may come when the courage of men fails, when we forsake our friends and break all bonds of fellowship, but it is not this day. An hour of wolves and shattered shields, when the age of men comes crashing down, but it is not this day! This day we fight! By all that you hold dear on this good Earth, I bid you stand, Men of the West!`,
   `Saruman believes it is only great power that can hold evil in check. But that is not what I have found. I've found it is the small things, everyday deeds of ordinary folk that keeps the darkness at bay. Simple acts of kindness and love.`,
   `It's like in the great stories, Mr. Frodo. The ones that really mattered, full of darkness and danger they were, and sometimes you didn't want to know the end, because how could the end be happy? How could the world go back to the way it was when so much bad had happened? But in the end, it's only a passing thing, this shadow. Even darkness must pass. A new day will come, and when the sun shines it will shine out the clearer. Those were the stories that stayed with you, that meant something, even if you were too small to understand why. But I think, Mr. Frodo, I do understand. I know now. Folk in those stories had lots of chances of turning back, only they didn't. They kept going, because they were holding on to something. What are we holding on to Sam? That there is some good in this world, and it's worth fighting for.`,
+  /* `Hi`,
   `Hi`,
   `Hi`,
-  `Hi`,
-  `Hi` /* --these are to make testing easier */,
+  `Hi`  --these are to make testing easier */
 ];
 let currentSentence: string;
+let currentSentenceIndex: number;
 let done = false;
-// const completedArray = [];
-// const lotrQuotePositions = [0, 1, 2];
+const completedArray: number[] = [];
+const lotrQuotePositions = [0, 1, 2];
 let currentPlace = 0;
 let accurateInputs = 0;
 let inaccurateInputs = 0;
@@ -30,9 +31,11 @@ const $error = document.querySelector('.error');
 const $palimpsest = document.querySelector('.palimpsest');
 const $accuracy = document.querySelector('#accuracy');
 const $reset = document.querySelector('#reset') as HTMLDialogElement;
+const $lotrAward = document.querySelector('.lotr');
 
 function setUp(): void {
-  currentSentence = sentences[Math.floor(Math.random() * sentences.length)];
+  currentSentenceIndex = Math.floor(Math.random() * sentences.length);
+  currentSentence = sentences[currentSentenceIndex];
   currentPlace = 0;
   if (!$yeomansWork) throw new Error('$yeomansWork is missing!');
   $yeomansWork.textContent = '';
@@ -93,5 +96,26 @@ function updateAccuracy(): void {
 
 function reset(): void {
   done = false;
+  if (
+    Math.round((accurateInputs / (inaccurateInputs + accurateInputs)) * 100) >=
+      95 &&
+    !completedArray.includes(currentSentenceIndex)
+  ) {
+    completedArray.push(currentSentenceIndex);
+  }
+  addAwards();
   setUp();
+}
+
+function addAwards(): void {
+  let lotrMatches = 0;
+  for (let i = 0; i < lotrQuotePositions.length; i++) {
+    if (completedArray.includes(lotrQuotePositions[i])) lotrMatches++;
+  }
+  console.log('lQP.length:', lotrQuotePositions.length);
+  console.log('lotrMatches:', lotrMatches);
+  if (lotrMatches === lotrQuotePositions.length) {
+    if (!$lotrAward) throw new Error('$lotrAward note found!');
+    $lotrAward.className = 'award-column lotr';
+  }
 }
