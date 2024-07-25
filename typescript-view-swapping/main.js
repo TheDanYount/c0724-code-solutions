f06408ec8eb49cc67c59d71bf5f6df4f1da0328e
Daniel Yount <TheDanYount@gmail.com>
2024-07-25 20:53:53 +0000

    Applied best practices
    Did querySelectors ahead of time as to not slow down the page when buttons are clicked. Added guard statement. Added console log to guard statement so my %0^#^ing sytanx-checker will not consider it an empty code block).

--- typescript-view-swapping/main.js
+++ typescript-view-swapping/main.js
@@ -1,18 +1,23 @@
 'use strict';
 const $tabContainer = document.querySelector('.tab-container');
+const $viewClassNodeList = document.querySelectorAll('.view');
+const $tabClassNodeList = document.querySelectorAll('.tab');
 if (!$tabContainer) throw new Error('$tabContainer not found!');
 $tabContainer.addEventListener('click', clickHandler);
 function clickHandler(event) {
   const eventTarget = event.target;
-  if (eventTarget.matches('.tab')) {
-    for (const tab of document.querySelectorAll('.tab')) {
+  if (!eventTarget.matches('.tab')) {
+    console.log('Tab not clicked.');
+    return;
+  } else {
+    for (const tab of $tabClassNodeList) {
       if (tab === eventTarget) {
         tab.className = 'tab active';
       } else {
         tab.className = 'tab';
       }
     }
-    for (const view of document.querySelectorAll('.view')) {
+    for (const view of $viewClassNodeList) {
       if (!(view instanceof HTMLElement))
         throw new Error('view not HTMLElement');
       if (view.dataset.view === eventTarget.dataset.view) {
--- typescript-view-swapping/main.ts
+++ typescript-view-swapping/main.ts
@@ -1,18 +1,22 @@
 const $tabContainer = document.querySelector('.tab-container');
+const $viewClassNodeList = document.querySelectorAll('.view');
+const $tabClassNodeList = document.querySelectorAll('.tab');
 if (!$tabContainer) throw new Error('$tabContainer not found!');
 $tabContainer.addEventListener('click', clickHandler);
 
 function clickHandler(event: Event): void {
   const eventTarget = event.target as HTMLElement;
-  if (eventTarget.matches('.tab')) {
-    for (const tab of document.querySelectorAll('.tab')) {
+  if (!eventTarget.matches('.tab')) {
+    console.log('Tab not clicked.');
+  } else {
+    for (const tab of $tabClassNodeList) {
       if (tab === eventTarget) {
         tab.className = 'tab active';
       } else {
         tab.className = 'tab';
       }
     }
-    for (const view of document.querySelectorAll('.view')) {
+    for (const view of $viewClassNodeList) {
       if (!(view instanceof HTMLElement))
         throw new Error('view not HTMLElement');
       if (view.dataset.view === eventTarget.dataset.view) {
