e1ba2a1ded9293ec5e2c9a21e5744341e070dc63
Daniel Yount <TheDanYount@gmail.com>
2024-07-25 21:08:20 +0000

    Removed else couching for post-guard-clause

--- typescript-view-swapping/main.js
+++ typescript-view-swapping/main.js
@@ -7,24 +7,21 @@ $tabContainer.addEventListener('click', clickHandler);
 function clickHandler(event) {
   const eventTarget = event.target;
   if (!eventTarget.matches('.tab')) {
-    // console.log('Tab not clicked.');
     return;
-  } else {
-    for (const tab of $tabClassNodeList) {
-      if (tab === eventTarget) {
-        tab.className = 'tab active';
-      } else {
-        tab.className = 'tab';
-      }
+  }
+  for (const tab of $tabClassNodeList) {
+    if (tab === eventTarget) {
+      tab.className = 'tab active';
+    } else {
+      tab.className = 'tab';
     }
-    for (const view of $viewClassNodeList) {
-      if (!(view instanceof HTMLElement))
-        throw new Error('view not HTMLElement');
-      if (view.dataset.view === eventTarget.dataset.view) {
-        view.className = 'view';
-      } else {
-        view.className = 'view hidden';
-      }
+  }
+  for (const view of $viewClassNodeList) {
+    if (!(view instanceof HTMLElement)) throw new Error('view not HTMLElement');
+    if (view.dataset.view === eventTarget.dataset.view) {
+      view.className = 'view';
+    } else {
+      view.className = 'view hidden';
     }
   }
 }
--- typescript-view-swapping/main.ts
+++ typescript-view-swapping/main.ts
@@ -7,23 +7,21 @@ $tabContainer.addEventListener('click', clickHandler);
 function clickHandler(event: Event): void {
   const eventTarget = event.target as HTMLElement;
   if (!eventTarget.matches('.tab')) {
-    // console.log('Tab not clicked.');
-  } else {
-    for (const tab of $tabClassNodeList) {
-      if (tab === eventTarget) {
-        tab.className = 'tab active';
-      } else {
-        tab.className = 'tab';
-      }
+    return;
+  }
+  for (const tab of $tabClassNodeList) {
+    if (tab === eventTarget) {
+      tab.className = 'tab active';
+    } else {
+      tab.className = 'tab';
     }
-    for (const view of $viewClassNodeList) {
-      if (!(view instanceof HTMLElement))
-        throw new Error('view not HTMLElement');
-      if (view.dataset.view === eventTarget.dataset.view) {
-        view.className = 'view';
-      } else {
-        view.className = 'view hidden';
-      }
+  }
+  for (const view of $viewClassNodeList) {
+    if (!(view instanceof HTMLElement)) throw new Error('view not HTMLElement');
+    if (view.dataset.view === eventTarget.dataset.view) {
+      view.className = 'view';
+    } else {
+      view.className = 'view hidden';
     }
   }
 }
