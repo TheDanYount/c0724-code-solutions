eb5c11a199db33c09e4a5c297f3128c9cec74e74
Daniel Yount <TheDanYount@gmail.com>
2024-08-27 23:26:03 +0000

    Completed the exercise & quiz

---
+++ react-events/my-app/src/ButtonContainer.tsx
@@ -0,0 +1,12 @@
+type Props = {
+  label: string;
+  onButtonClick: () => void;
+};
+
+export function ButtonContainer({ label, onButtonClick }: Props) {
+  return (
+    <div>
+      <button onClick={onButtonClick}>{label}</button>
+    </div>
+  );
+}
