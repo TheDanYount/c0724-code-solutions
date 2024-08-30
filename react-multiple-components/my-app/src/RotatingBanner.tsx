88b7fd6466b973c0f476679ab886bf807d19d308
Daniel Yount <TheDanYount@gmail.com>
2024-08-30 18:59:24 +0000

    Added state
    Didn't pass state, as I only need to pass a string derived from state (which I do pass)

--- react-multiple-components/my-app/src/RotatingBanner.tsx
+++ react-multiple-components/my-app/src/RotatingBanner.tsx
@@ -3,15 +3,17 @@ import { Title } from './Title';
 import { Next } from './Next';
 import { ButtonRow } from './ButtonRow';
 import { Prev } from './Prev';
+import { useState } from 'react';
 
 type Props = {
   items: string[];
 };
 
 export function RotatingBanner({ items }: Props) {
+  const [stage, setStage] = useState(0);
   return (
     <>
-      <Title title={items[0]} />
+      <Title title={items[stage]} />
       <Next />
       <ButtonRow items={items} />
       <Prev />
